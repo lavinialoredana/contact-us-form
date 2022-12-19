a828c72685094611617f0548d818ce447a4cb6ec
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2022-12-19 10:06:57 +0100

    Define basic html structure for project

--- src/pages/AppPage/index.jsx
+++ src/pages/AppPage/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -5,8 +6,36 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> Contact Us</h1>
-        
       </header>
+
+      <div className="contact-form-main-container">
+        <div className="email-container">
+          <input 
+          type="text" 
+          label="E-mail" 
+          value=''
+          />
+        </div>
+
+        <div className="suffix-dropdown-list">
+          <select name="" id="">
+            <option selected disabled hidden>
+              Select an option
+            </option>
+            <option>Mr.</option>
+            <option> Mrs.</option>
+            <option> Miss</option>
+          </select>
+        </div>
+
+        <div className="request-description-container">
+          <input type="text" label="request-description" value=""></input>
+        </div>
+
+        <div className="submit-request-button">
+          <button> Submit </button>
+        </div>
+      </div>
     </div>
   );
 }
