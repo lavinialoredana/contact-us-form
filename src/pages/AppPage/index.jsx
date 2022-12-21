9ce9b76c18e7764c5d5c6b17daffb9263d55659e
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2022-12-21 15:48:36 +0200

    Fetch Beer from api

--- src/pages/AppPage/index.jsx
+++ src/pages/AppPage/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import InsertDescriptionField from "../../components/Description";
 import EmailField from "../../components/CustomerEmail";
 import SubmitButton from "../../components/SubmitButton";
@@ -6,13 +6,16 @@ import SuffixDropdownList from "../../components/SuffixDropdownListComponent";
 import "./app.css";
 import { v4 as uuidv4 } from "uuid";
 import CustomerMessage from "../../components/EmailAndDescriptionMessageList";
-
+import Axios from "axios";
 
 function App() {
   const [email, setEmail] = useState("");
   const [descriptionRequest, setDescriptionRequest] = useState("");
   const [emailAndDescriptionRequestList, setEmailAndDescriptionRequestList] =
     useState([]);
+  const [data, setData] = useState();
+
+
   const onEmailChange = (event) => {
     setEmail(event.target.value);
   };
@@ -33,8 +36,19 @@ function App() {
     setEmail("");
     setDescriptionRequest("");
 
-    console.log("myResultList", emailAndDescriptionRequestList);
+    // console.log("myResultList", emailAndDescriptionRequestList);
   };
+
+  const fetchRandomBeer = useCallback(async () => {
+    const response = await Axios.get("https://api.punkapi.com/v2/beers/192");
+    console.log("fetchRandomBeer");
+    setData(response.data[0]);
+  }, []);
+
+  useEffect(() => {
+    fetchRandomBeer();
+    console.log("rerender", email, descriptionRequest)
+  }, []);
 
   return (
     <div className="App">
@@ -53,6 +67,7 @@ function App() {
         <CustomerMessage
           emailAndDescriptionRequestList={emailAndDescriptionRequestList}
         />
+        {data && data.image_url && <img src={data.image_url} alt="beer"></img>}
       </div>
     </div>
   );
